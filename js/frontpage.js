00758a4dc50c65fd793781e7762abab3c9ef51b8
Chowdhury-Joy <62534869+Chowdhury-Joy@users.noreply.github.com>
2021-03-25 16:42:00 +0600

    banner improved & gsap added a bit

--- js/frontpage.js
+++ js/frontpage.js
@@ -2,11 +2,22 @@ var tl = gsap.timeline({
 
 });
 
-tl.from('.nav-whole', {opacity: 0,duration: 1, yPercent: -100})
-.from('.bannerImg', {opacity: 0,duration: 1, yPercent: -100})
-.from('#scrldwn01', {opacity: 0,duration: 1, y: -50})
-.from('.scrl-stagger', 2, {opacity: 0, y: 50, ease: Expo.easeInOut, stagger: 0.5}, "-=1")
+//!!BANNER
 
-// .from('#scrldwn01', {opacity: 0,duration: 1, y: -50, stagger: 0.6})
-// .from('#scrldwn01', {opacity: 0,duration: 1, y: -50, stagger: 0.6})
-// .from('#scrldwn02', {opacity: 0,duration: 1, y: -50, stagger: 0.6})
+// tl.from('.nav-center', 1, {delay: .2, opacity: 0, x: -20, ease: Expo.easeInOut})
+// .from('.nav-right', 1, {delay: .1, opacity: 0, x: -20, ease: Expo.easeInOut})
+// tl.from('.nav-whole', {opacity: 0,duration: 1, xPercent: -100, stagger: 0.5})
+tl.from('.bannerImg', {opacity: 0,duration: 1, xPercent: -100})
+.from('#scrldwn01', {opacity: 0,duration: 1, y: -50})
+.from('.scrl-stagger', 2, {opacity: 0, y: 50, ease: Expo.easeInOut, stagger: 0.5},"<")
+
+//!! CTI or EXPLORE
+
+.to(".cti", {
+          scrollTrigger: {
+                    trigger: '.cti-all',
+                    start: "top center",
+                    
+          },
+          y: -50
+})
